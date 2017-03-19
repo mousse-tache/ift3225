c9dd496ba17130a2b0e72f1ee1c730bb6bdabad8
William Neveu <williamneveu@gmail.com>
2017-03-19 15:36:06 -0400

    allo

--- tp2/public/js/form.js
+++ tp2/public/js/form.js
@@ -21,8 +21,28 @@ $(document).ready(function(){
 
 
 	$('#brassertuiles').click(function() {
-		var table = getElementById('gametable');
+		$('#tablewrapper *').remove();
 		
+		var r = $('#rows').val();
+		var c = $('#columns').val();
+		
+		
+		var width = 780/c+'px';
+		var height=380/r+'px';
+		var back = 'url('+url+')';
+		var table = generateGrid(c,r,url);
+		shuffle(table, r, c, 15);
+		$('#tablewrapper').append(table);
+
+		$('#tablewrapper td').css({'width':width,
+			'height':height,
+			'background-image':back});
+		$('#tablewrapper span').addClass('hidden');
+
+
+
+		$('#tablewrapper table').attr('id', 'gametable')
+		shuffle($('#gametable'),r,c, 15);
 	});
 
 	$('#afficher').click(function() {
@@ -47,6 +67,8 @@ $(document).ready(function(){
 
 
 		$('#tablewrapper table').attr('id', 'gametable')
+		
+		
 	});
 
 	var gameActions = function() {
@@ -55,14 +77,62 @@ $(document).ready(function(){
 		Possible actions = {'Up', 'Right', 'Down', 'Left'}
 		*/
 	}
+	
 
-	var shuffle = function(table, v) {
+	var shuffle = function(table,r,c, v) {
 		/*
 		@param table : table that the cells will be shuffled
-		@param v : float between 0 and 1 which determines the probability each cell will be shuffled
+		@param v : float between 0 and 1 w,hich determines the probability each cell will be shuffled
 		return: shuffled table
 		*/
+		var grisR = r-1;
+		var grisC = c-1;
+		var ordre = new Array(r);
+		var x, y;
+		var tmp, parnt;
+		var i,j;
+		for (i=0; i<r; i++) {
+			ordre[i] = new Array(c);
+		}
+		for (i=0; i<r; i++) {
+			for (j =0; j<c; j++) {
+		 		ordre[i][j] = (j+1) + i*c;
+			};
+		};
+
+
+		for(var i = 0; i < 15; i++) {
+			x = Math.floor(Math.random() * 2);
+			if (x == 0) x--;
+			y = Math.floor(Math.random() * 2);
+			if (y == 0) x--;
+			
+			if (0 > x + grisR || x + grisR >= r || 0 > y + grisC || y + grisC >= c) {continue;}
+
+			tmp = ordre[grisR][grisC];
+			ordre[grisR][grisC] = ordre[grisR + x][grisC + y];
+			ordre[grisR + x][grisC + y] = tmp;
+			
+			console.log(table.childNodes[grisR].childNodes[grisC]);
+			
+			tmp = table.childNodes[grisR].childNodes[grisC];
+			
+			parnt = table.childNodes[grisR].childNodes[grisC].parentNode;
+			parnt.replaceChild(table.childNodes[grisR+x].childNodes[grisC+y], table.childNodes[grisR].childNodes[grisC])
+			
+			//table.childNodes[grisR].childNodes[grisC] = table.childNodes[grisR+x].childNodes[grisC+y];
+			
+			parnt = table.childNodes[grisR+x].childNodes[grisC+y].parentNode
+			parnt.replaceChild(tmp, table.childNodes[grisR+x].childNodes[grisC+y])
+			
+			//table.childNodes[grisR+x].childNodes[grisC+y] = tmp;
+			grisR = grisR+x;
+			grisC = grisC+y;
+		}
+
+ 		//return table;
 	}
+
 
 	var winCheck = function(table) {
 		//Iterates over cells. If they're in the right order, returns true else false
@@ -91,6 +161,7 @@ $(document).ready(function(){
 		    table.appendChild(tr);
 
 		}
+		
 		return table
 	}
 
